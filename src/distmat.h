cd1d890311fbb551494aded72791d91d8ba8cc8f
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2023-12-22 14:54:36 +0100

    convert symmat to distmat (we don't need to keep track of the diagonal elements in the distance matrix)

---
+++ src/distmat.h
@@ -0,0 +1,22 @@
+#include <vector>
+
+#ifndef DISTMAT_H
+#define DISTMAT_H
+
+template <class T>
+class distmat
+{
+public:
+  distmat(int n = 0, T def = T());
+  distmat(const std::vector<T> &vec);
+  T &acc(const int i, const int j);
+  T get(const int i, const int j) const;
+  int size() const;
+
+private:
+  std::vector<T> data;
+  int m_size;
+
+};
+
+#endif
