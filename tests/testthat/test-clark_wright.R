42381dfc6661b3575600f46849b3b37491131a6d
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2023-12-31 15:00:05 +0100

    add simple test to catch simple errors

--- tests/testthat/test-clark_wright.R
+++ tests/testthat/test-clark_wright.R
@@ -1,3 +1,21 @@
+test_that("runs without error", {
+  demand <- c(1, 1)
+
+  pos <-
+    data.frame(
+      pos_x = c(0, 1, -1),
+      pos_y = c(0, 1, 1)
+    )
+
+  expect_no_error(
+    clark_wright(
+      demand,
+      dist(pos),
+      data.frame(n = NA_integer_, caps = 99999)
+    )
+  )
+})
+
 test_that("vehicle with infinite capacity covers everything in a single run", {
   hedgehog::forall(
     gen.demand_net(max_sites = 10L),
