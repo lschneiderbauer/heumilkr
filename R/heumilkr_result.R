bb527748d6b233ff22a6eab49a5f708469aa7cd3
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-01-01 18:51:39 +0100

    add S3 heumilkr_result object

---
+++ R/heumilkr_result.R
@@ -0,0 +1,18 @@
+heumilkr_result <- function(df, distances) {
+  stopifnot(inherits(df, "data.frame"))
+  stopifnot(c("run_id", "order", "vehicle_id") %in% colnames(df))
+  stopifnot(inherits(distances, "dist"))
+
+  new_heumilkr_result(df, distances)
+}
+
+new_heumilkr_result <- function(df, distances) {
+  obj <-
+    structure(
+      df,
+      class = c("heumilkr_result", class(df))
+    )
+
+  attr(obj, "distances") <- distances
+  obj
+}
