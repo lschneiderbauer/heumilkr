78ef9ebf895674b28d37fef4a5352e881bf022ea
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-01-02 21:49:19 +0100

    add more clarke-wright tests

---
+++ tests/testthat/_snaps/clarke_wright.md
@@ -0,0 +1,27 @@
+# README example result is preserved.
+
+    Code
+      res
+    Output
+         run order vehicle
+      1    0     0       0
+      2    1     0       0
+      3    0     2       0
+      4    2     1       0
+      5    1     1       0
+      6    3     6       0
+      7    3     8       0
+      8    3     4       0
+      9    3     7       0
+      10   3     3       0
+      11   3     1       0
+      12   3     2       0
+      13   4     0       0
+      14   3     5       0
+      15   4     2       0
+      16   2     0       0
+      17   4     3       0
+      18   4     1       0
+      19   0     1       0
+      20   3     0       0
+
--- tests/testthat/test-clarke_wright.R
+++ tests/testthat/test-clarke_wright.R
@@ -16,7 +16,7 @@ test_that("runs without error", {
   )
 })
 
-test_that("vehicle with infinite capacity covers everything in a single run", {
+test_that("A vehicle with infinite capacity covers everything in a single run", {
   hedgehog::forall(
     gen.demand_net(max_sites = 10L),
     function(demand_net) {
@@ -26,8 +26,66 @@ test_that("vehicle with infinite capacity covers everything in a single run", {
           demand_net$distances, data.frame(n = NA_integer_, caps = 99999)
         )
 
-      expect_equal(unique(res$run_id), 0)
-      expect_equal(unique(res$vehicle_id), 0)
+      expect_equal(unique(res$run), 0)
+      expect_equal(unique(res$vehicle), 0)
     }
   )
+})
+
+test_that("A demand that exceeds vehicle capacities generates more than a single run", {
+  demand <- c(15)
+
+  pos <-
+    data.frame(
+      pos_x = c(0, 1),
+      pos_y = c(0, 1)
+    )
+
+  res <-
+    clarke_wright(
+      demand,
+      dist(pos),
+      data.frame(n = NA_integer_, caps = 6)
+    )
+
+  expect_equal(length(unique(res$run)), 3)
+  expect_equal(unique(res$site), 0)
+})
+
+test_that("Not having enough vehicles is handled gracefully", {
+  demand <- c(15, 3, 1)
+
+  pos <-
+    data.frame(
+      pos_x = c(0, 1, -1, -2),
+      pos_y = c(0, 1, 1, 2)
+    )
+
+  expect_error(
+    clarke_wright(
+      demand,
+      dist(pos),
+      data.frame(n = 2L, caps = 6)
+    )
+  )
+})
+
+test_that("README example result is preserved.", {
+  set.seed(42)
+  demand <- runif(20, 5, 15)
+
+  pos <-
+    data.frame(
+      pos_x = c(0, runif(length(demand), -10, 10)),
+      pos_y = c(0, runif(length(demand), -10, 10))
+    )
+
+  res <-
+    clarke_wright(
+      demand,
+      dist(pos),
+      data.frame(n = NA_integer_, caps = 33)
+    )
+
+  expect_snapshot(res)
 })
