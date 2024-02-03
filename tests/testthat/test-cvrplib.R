4f30ee7837b7bfc232c5c48182cad7eb7e0fcb23
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-02-03 15:40:21 +0100

    make cvrplib download test stricter

--- tests/testthat/test-cvrplib.R
+++ tests/testthat/test-cvrplib.R
@@ -7,11 +7,18 @@ test_that("Applying clarke_wright to cvrplib data results in a valid solution ob
   )
 })
 
-test_that("CVRP downloader yields a result", {
+test_that("CVRP downloader yields correct result", {
   skip_if_offline()
 
+  inst <- cvrplib_download("A/A-n80-k10")
+
   expect_s3_class(
-    cvrplib_download("A/A-n80-k10"),
+    inst,
     "cvrplib_instance"
+  )
+
+  expect_equal(
+    inst,
+    cvrplib_A[[27]]
   )
 })
