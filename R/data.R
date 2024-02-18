1fb53058a9e4cef1401aa3613f427cd551524f88
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2024-02-18 19:01:52 +0100

    fix typo

--- R/data.R
+++ R/data.R
@@ -1,8 +1,8 @@
 #' CVRP instance data by Augerat, 1995
 #'
 #' A collection of CVRP instances by Augerat, 1995, provided courtesy of
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
 #' instances and their solutions as well as a multitude of alternative instance
 #' data.
 #'
@@ -16,8 +16,8 @@
 #' CVRP instance data by Augerat, 1995
 #'
 #' A collection of CVRP instances by Augerat, 1995, provided courtesy of
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
 #' instances and their solutions as well as a multitude of alternative instance
 #' data.
 #'
@@ -31,8 +31,8 @@
 #' CVRP instance data by Christofides and Eilon, 1969
 #'
 #' A collection of CVRP instances by Christofides and Eilon, 1969, provided courtesy of
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
 #' instances and their solutions as well as a multitude of alternative instance
 #' data.
 #'
@@ -46,8 +46,8 @@
 #' CVRP instance data by Fisher, 1994
 #'
 #' A collection of CVRP instances by Fisher, 1994, provided courtesy of
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
 #' instances and their solutions as well as a multitude of alternative instance
 #' data.
 #'
@@ -61,8 +61,8 @@
 #' CVRP instance data by Rochat and Taillard, 1995
 #'
 #' A collection of CVRP instances by Rochat and Taillard, 1995, provided courtesy of
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
-#' [CBRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br). See
+#' [CVRPLIB](http://vrp.atd-lab.inf.puc-rio.br) for visualizations of the
 #' instances and their solutions as well as a multitude of alternative instance
 #' data.
 #'
