4ec105f48a96e1cf211be80b6667c282edcfc7f0
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2025-12-29 23:27:35 +0100

    c++ code: factor out run class

---
+++ src/run.h
@@ -0,0 +1,22 @@
+#include <vector>
+#include <memory>
+#include <unordered_set>
+
+#ifndef RUN_H
+#define RUN_H
+
+// a run consists of an _unordered_ collection of site
+class run
+{
+  public:
+    std::unordered_set<int> sites;
+    double max_load;
+    int vehicle;
+
+    run(int site, double max_load); // initialize a run with a single site
+    void combine(run &other_run, int new_vehicle);
+    
+  private:
+};
+
+#endif
--- src/udg.h
+++ src/udg.h
@@ -1,6 +1,7 @@
-#include <unordered_set>
 #include <vector>
 #include <memory>
+#include <unordered_set>
+#include "run.h"
 
 #ifndef UDG_H
 #define UDG_H
@@ -9,19 +10,20 @@
 class udg
 {
 public:
-  udg(int n_vertices = 0);
-  void add_edge(const int a, const int b);
-  void relink_edge(const int a, const int b);
+  udg(const std::vector<double> demand);
+  void combine_runs(const int a, const int b, const int new_vehicle);
   bool links_to_origin(const int a) const;
   bool edges_share_cycle(const int a, const int b) const;
-  std::unordered_set<int> sites_in_cycle(const int a) const;
-  std::vector<std::shared_ptr<std::unordered_set<int>>> get_cycs() const;
+  // a vector of runs (of length of the sites): each site has a reference to
+  // the runs it belongs to (which in turn has all the other references)
+  std::vector<std::shared_ptr<run>> runs;
 
 private:
+  static constexpr int ORIGIN = -1;
+  void relink_edge(const int a, const int b);
+  void add_edge(const int a, const int b);
   std::vector<std::unordered_set<int>> adj;
-  // a vector of cycles: each vertex has a reference to
-  // the cycle it belongs to (which in turn has all the other references)
-  std::vector<std::shared_ptr<std::unordered_set<int>>> cycs;
 };
+
 
 #endif
