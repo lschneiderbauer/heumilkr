76d643d0f30d7c1123b400f16270fd902a0e23bd
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2025-12-30 12:03:28 +0100

    consistency

--- src/run.cpp
+++ src/run.cpp
@@ -12,7 +12,7 @@ void run::combine(run &other_run, int new_vehicle)
     sites.merge(other_run.sites);
 
     // new max load
-    double new_max_load = run::max_load + other_run.max_load;
+    double new_max_load = this->max_load + other_run.max_load;
     this->max_load = new_max_load;
     other_run.max_load = new_max_load;
 
