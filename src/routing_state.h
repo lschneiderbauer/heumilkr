ebc3ce43a94ffa6883217fc758439ced6aabad50
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2026-01-01 23:27:16 +0100

    make the last vehicle reassignment run a bit smarter

--- src/routing_state.h
+++ src/routing_state.h
@@ -25,7 +25,7 @@ public:
                 const std::vector<std::unordered_set<int>> &restricted_vehicles);
   col_types runs_as_cols() const;
   bool relink_best();
-  bool opt_vehicles();
+  void opt_vehicles();
 
 private:
   distmat<double> distances;
