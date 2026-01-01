c60799e899c6b150010b9f9d3a745ad31cb0a301
Lukas Schneiderbauer <lukas.schneiderbauer@gmail.com>
2026-01-01 23:16:16 +0100

    factor out fleet type

--- src/routing_state.h
+++ src/routing_state.h
@@ -1,11 +1,11 @@
-#include "distmat.h"
-#include "udg.h"
-#include <stdint.h>
-#include <unordered_set>
-#include <vector>
-
 #ifndef ROUTINGSTATE_H
 #define ROUTINGSTATE_H
+
+#include "distmat.h"
+#include "udg.h"
+#include "fleet.h"
+#include <unordered_set>
+#include <vector>
 
 using col_types = std::tuple<
     std::vector<int>,
@@ -20,7 +20,7 @@ class routing_state
 public:
   routing_state(const std::vector<double> demand,
                 const distmat<double> &dist,
-                const std::vector<int> &vehicle_avail,
+                std::vector<int> vehicle_avail,
                 const std::vector<double> &vehicle_caps,
                 const std::vector<std::unordered_set<int>> &restricted_vehicles);
   col_types runs_as_cols() const;
@@ -29,16 +29,10 @@ public:
 
 private:
   distmat<double> distances;
-  // Number and capacity of particular vehicle types
-  std::vector<int> vehicle_avail;
-  std::vector<double> vehicle_caps;
-
-  std::vector<std::unordered_set<int>> restricted_vehicles;
+  Fleet fleet;
 
   distmat<double> savings;
   udg graph;
-  // maximum load of run that traverses site,
-  // (stored per site)
 
   // indexed by vehicle x site
   std::vector<std::vector<int>> singleton_runs;
